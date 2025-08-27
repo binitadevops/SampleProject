e84bfa9f8dc7f4f76d5b9675d1e3b209fd5c9f92
Binita <binita.devops@outlook.com>
2025-08-27 13:54:45 -0500

    1st commit on 08/27

--- SampleProjectLocal/src/Pack1/Test.java
+++ SampleProjectLocal/src/Pack1/Test.java
@@ -12,6 +12,8 @@ public class Test {
 		System.out.println("Changes 3 done on 08/12/2025");
 		System.out.println("Working on merge 1");
 		System.out.println("This is on branch 2");
+		System.out.println("Today is 08/27/2025 - Change 1");
+		
 	}
 
 }
